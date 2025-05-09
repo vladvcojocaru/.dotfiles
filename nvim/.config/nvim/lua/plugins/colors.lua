aaa8013eee732bf652662afacdb7f6a239343270
vlad <vladvcojocaru@gmail.com>
2025-05-09 21:24:06 +0300

    sum stuff

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -1,5 +1,5 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine"
+	color = color
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
@@ -12,47 +12,8 @@ return {
         lazy = false,
         priority = 1000,
         init = function()
-            -- vim.cmd.colorscheme("lackluster")
-            -- vim.cmd.colorscheme("lackluster-hack") -- my favorite
-            -- vim.cmd.colorscheme("lackluster-mint")
         end,
     },
-    -- Other plugins here...
-
-    -- {
-    --     'navarasu/onedark.nvim',
-    --     config = function()
-    --         require('onedark').setup {
-    --             -- Main options --
-    --             style = 'darker',     -- Change this to your preferred style
-    --             transparent = false,  -- Set background transparency
-    --             term_colors = true,   -- Match terminal colors
-    --             ending_tildes = true, -- Show tildes at the end of buffers
-    --             code_style = {
-    --                 comments = 'italic',
-    --                 keywords = 'bold',
-    --                 functions = 'italic',
-    --                 strings = 'none',
-    --                 variables = 'none',
-    --             },
-    --             diagnostics = {
-    --                 darker = true,
-    --                 undercurl = true,
-    --                 background = true,
-    --             },
-    --         }
-    --         require('onedark').load()
-    --     end
-    -- },
-    -- {
-    --     "olimorris/onedarkpro.nvim",
-    --     priority = 1000, -- Ensure it loads first
-    --     init = function()
-    --         vim.cmd("colorscheme onedark")
-    --     end
-    --
-    --
-    -- },
     -- {
     --     "rose-pine/neovim",
     --     name = "rose-pine",
@@ -80,7 +41,7 @@ return {
         priority = 1000,
         config = function ()
             vim.cmd("colorscheme tokyonight")
-            --ColorMyPencils("catppuccin-mocha")
+            ColorMyPencils("tokyonight")
         end
     }
 }
